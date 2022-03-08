3dc695bd59b9882dd777ea02793b8f012a2d3fa5
IlConteCvma <mar.calavaro@gmail.com>
2022-03-08 16:37:51 +0100

    klist

---
+++ driver/klist.h
@@ -0,0 +1,32 @@
+#include <linux/mutex.h>
+#include <linux/slab.h>
+/* klist metadata fifo rule */
+
+
+typedef struct _klist_elem{
+    char* buffer;
+    int last_read;
+    int size;
+    struct _klist_elem* next; 
+}klist_elem;
+
+
+typedef struct _klist
+{
+    struct mutex op_mtx;
+    klist_elem* head;
+    klist_elem* tail;
+
+
+}klist;
+
+/*Function*/
+
+extern klist klist_alloc(void);
+extern void klist_free(klist*);
+extern unsigned int klist_put(klist*,const char*,int);
+extern unsigned int klist_get(klist*,char*,int);
+extern unsigned int klist_len(klist*);
+
+klist_elem klist_elem_alloc(char*,int);
+void klist_elem_free(klist_elem*);
